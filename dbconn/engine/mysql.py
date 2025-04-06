ca93b6a3619f6cfca951467b925de71f6933e63d
Rob Moore <giftiger.wunsch@gmail.com>
2025-04-06 19:06:21 +0100

    Use mycli as default mysql client
    Knew there had to be a better one than the standard mysql client

--- dbconn/engine/mysql.py
+++ dbconn/engine/mysql.py
@@ -3,8 +3,8 @@ from dbconn.engine import Engine, Executable
 
 
 class MysqlExecutable(Executable):
-    def __init__(self, prompt_text: str | None = None):
-        self.prompt_text = prompt_text
+    def __init__(self, prompt: str | None = None):
+        self.prompt = prompt
 
     def get_command(self, db: Database, host: str | None = None, port: int | None = None):
         cmd = [
@@ -18,13 +18,35 @@ class MysqlExecutable(Executable):
             str(port),
             f"--database={db.database}",
         ]
-        if self.prompt_text:
-            cmd.append(f"--prompt={self.prompt_text}> ")
+        if self.prompt:
+            cmd.append(f"--prompt={self.prompt}")
+
+        return cmd
+
+
+class MycliExecutable(Executable):
+    def __init__(self, prompt: str | None = None):
+        self.prompt = prompt
+
+    def get_command(self, db: Database, host: str | None = None, port: int | None = None):
+        cmd = [
+            "mycli",
+            "-u",
+            db.user,
+            "-h",
+            host or db.host,
+            f"--password={db.password}",
+            "--port",
+            str(port),
+            f"--database={db.database}",
+        ]
+        if self.prompt:
+            cmd.append(f"--prompt={self.prompt}")
 
         return cmd
 
 
 class MysqlEngine(Engine):
-    supported_executables = {"mysql": MysqlExecutable}
-    default_executable = "mysql"
+    supported_executables = {"mysql": MysqlExecutable, "mycli": MycliExecutable}
+    default_executable = "mycli"
     default_port = 3306
